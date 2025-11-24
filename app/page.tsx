682061f9f2fd0bbbe012ac0e3c59584f3d663f18
Alex Cristoffanini <acristoffanini@gmail.com>
2025-11-24 10:53:58 -0800

    data entry

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,11 @@
 import { Button } from "@/components/ui/button";
-import Image from "next/image";
+
+
+
+
+
+
+
 
 export default function Home() {
   return (
@@ -8,3 +14,41 @@ export default function Home() {
     </div>
   );
 }
+
+const cableData = [
+  {
+    "COLBI_ID": "COLBI_ID_0007681",
+    "ID": "17254",
+    "STOCK_NUM": "105460",
+    "SPEC_NAME": "AL 600V XLPE Insulation Single Type USE-2",
+    "OVERALL_DIAMETER": 0.512, //inches
+    "CONDUCTOR_NUMBER": 3
+  },
+  {
+    "COLBI_ID": "COLBI_ID_0001988",
+    "ID": "47101",
+    "STOCK_NUM": "138929",
+    "SPEC_NAME": "CU 1000V XLPE Insulation Teck 90 LSZH Jacket",
+    "OVERALL_DIAMETER": 0.899,
+    "CONDUCTOR_NUMBER": 2
+  },
+  {
+    "COLBI_ID": "COLBI_ID_0005592",
+    "ID": "4110",
+    "STOCK_NUM": "646907",
+    "SPEC_NAME": "CU 15kV NLEPR Insulation 133% IL AIA Red CPE-TP Jacket.  Tray Rated - Sunlight Resistant - For Direct Burial",
+    "OVERALL_DIAMETER": 3.443,
+    "CONDUCTOR_NUMBER": 1
+  },
+]
+
+const Conduits = [
+  {
+    "INNER_CONDUIT_DIAMETER": 2.047
+  },
+  {
+    "INNER_CONDUIT_DIAMETER": 6.031
+  }
+]
+
+
